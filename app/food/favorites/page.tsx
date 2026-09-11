4ee63e0a19d7e9e4ffe81e855a9c8be959512fcd
JIDAIN <43210783+JIDAIN@users.noreply.github.com>
2026-09-11 14:57:12 +0800

    feat(food): route frequent food management

---
+++ app/food/favorites/page.tsx
@@ -0,0 +1,10 @@
+import { LifeAppShell } from "@/components/life/LifeAppShell";
+import { LifeFavoriteFoodsPage } from "@/components/life/LifeFavoriteFoodsPage";
+
+export default function FavoriteFoodsPage() {
+  return (
+    <LifeAppShell>
+      <LifeFavoriteFoodsPage />
+    </LifeAppShell>
+  );
+}
